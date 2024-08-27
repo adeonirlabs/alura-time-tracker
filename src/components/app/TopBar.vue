6348abc3a6369b622ceb240285e211fd1cc21e2e
Adeonir Kohl <adeonir@gmail.com>
2024-08-27 13:03:34 -0300

    Create input component

--- src/components/app/TopBar.vue
+++ src/components/app/TopBar.vue
@@ -2,12 +2,14 @@
 import { Play, Square } from 'lucide-vue-next'
 
 import ActionButton from '@/components/ui/ActionButton.vue'
+import InputField from '@/components/ui/InputField.vue'
 import TimerClock from '@/components/ui/TimerClock.vue'
 
 export default {
   name: 'TopBar',
   components: {
     ActionButton,
+    InputField,
     Play,
     Square,
     TimerClock,
@@ -37,11 +39,7 @@ export default {
 <template>
   <header class="w-full p-6">
     <div class="flex items-center gap-8 rounded-lg bg-white p-3 shadow">
-      <input
-        type="text"
-        class="block flex-1 rounded border-0 bg-transparent px-2 py-1 text-zinc-900 outline-none ring-zinc-300 transition-all hover:ring-1 focus:ring-1 focus:ring-indigo-600"
-        placeholder="No que você está trabalhando?"
-      />
+      <InputField placeholder="No que você está trabalhando?" />
       <TimerClock :timer="timer" />
       <ActionButton v-if="isRunning" variant="secondary" @click="stopTimer">
         <Square :size="16" />
---
+++ src/components/ui/InputField.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+export default {
+  name: 'InputField',
+}
+</script>
+
+<template>
+  <input
+    type="text"
+    class="block flex-1 rounded border-0 bg-transparent px-2 py-1 text-zinc-900 outline-none ring-zinc-300 transition-all hover:ring-1 focus:ring-1 focus:ring-indigo-600"
+    v-bind="$attrs"
+  />
+</template>
