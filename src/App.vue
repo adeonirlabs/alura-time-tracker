2cbd26f9b4b0b80b5ab52c9df09330e8fe0964e3
Adeonir Kohl <adeonir@gmail.com>
2024-08-27 08:56:22 -0300

    Update app html structure and add sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
+<script lang="ts">
+import SideBar from '@/components/SideBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SideBar,
+  },
+}
+</script>
+
 <template>
-  <h1>Time Tracker</h1>
+  <main class="grid grid-cols-[1fr_4fr]">
+    <div>
+      <SideBar />
+    </div>
+    <div>Content</div>
+  </main>
 </template>
