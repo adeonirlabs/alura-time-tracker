47f158857b9fc57853a71a98d296f93cdd6b94a4
Adeonir Kohl <adeonir@gmail.com>
2024-08-27 15:52:55 -0300

    Create task item component

---
+++ src/components/ui/TaskItem.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+export default {
+  name: 'TaskItem',
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-between gap-3 border-b border-zinc-200 px-4 py-3">
+    <p class="text-zinc-800">Task description</p>
+    <!-- <p class="ml-auto text-zinc-800">09:34 - 10:51</p> -->
+    <strong class="font-medium text-zinc-800">01:17:00</strong>
+  </div>
+</template>
