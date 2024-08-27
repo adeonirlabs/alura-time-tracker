fdcbf72e5ec63d437850629d2e8602261c0318c3
Adeonir Kohl <adeonir@gmail.com>
2024-08-27 16:55:54 -0300

    Refactor input component to handle the v-model correctly

--- src/components/ui/InputField.vue
+++ src/components/ui/InputField.vue
@@ -2,22 +2,28 @@
 export default {
   name: 'InputField',
   props: {
-    value: {
+    modelValue: {
       type: String,
       default: '',
     },
   },
-  data() {
-    return {
-      inputValue: this.value,
-    }
+  emits: ['update:modelValue'],
+  computed: {
+    value: {
+      get() {
+        return this.modelValue
+      },
+      set(value: string) {
+        this.$emit('update:modelValue', value)
+      },
+    },
   },
 }
 </script>
 
 <template>
   <input
-    v-model="inputValue"
+    v-model="value"
     type="text"
     class="block flex-1 rounded border-0 bg-transparent px-2 py-1 text-zinc-900 outline-none ring-zinc-300 transition-all hover:ring-1 focus:ring-1 focus:ring-indigo-600"
     v-bind="$attrs"
