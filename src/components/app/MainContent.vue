bc56c0f2af8c19a66ff784cbe49c2e80ff06523c
Adeonir Kohl <adeonir@gmail.com>
2024-08-27 16:58:23 -0300

    Add props to main content

--- src/components/app/MainContent.vue
+++ src/components/app/MainContent.vue
@@ -1,9 +1,25 @@
 <script lang="ts">
+import type { PropType } from 'vue'
+
+import TaskList from '@/components/app/TaskList.vue'
+import type { Task } from '@/types/task'
+
 export default {
   name: 'MainContent',
+  components: {
+    TaskList,
+  },
+  props: {
+    tasks: {
+      type: Array as PropType<Task[]>,
+      required: true,
+    },
+  },
 }
 </script>
 
 <template>
-  <div class="p-6">Main Content</div>
+  <section class="p-6 pt-0">
+    <TaskList :tasks="tasks" />
+  </section>
 </template>
