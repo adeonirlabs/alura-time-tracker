8119d1e8d14e9c8fa6731316bc0ead3f8608dc61
Adeonir Kohl <adeonir@gmail.com>
2024-08-27 15:04:51 -0300

    Update input field with data binding

--- src/components/ui/InputField.vue
+++ src/components/ui/InputField.vue
@@ -1,11 +1,23 @@
 <script lang="ts">
 export default {
   name: 'InputField',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      inputValue: this.value,
+    }
+  },
 }
 </script>
 
 <template>
   <input
+    v-model="inputValue"
     type="text"
     class="block flex-1 rounded border-0 bg-transparent px-2 py-1 text-zinc-900 outline-none ring-zinc-300 transition-all hover:ring-1 focus:ring-1 focus:ring-indigo-600"
     v-bind="$attrs"
