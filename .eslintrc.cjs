956e944a839fc24199bc5c1b62c1e8ced52ede7e
Adeonir Kohl <adeonir@gmail.com>
2024-08-27 07:45:51 -0300

    Install @forgedev-br/eslint-config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,35 +1,4 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  },
-  plugins: ['import', 'simple-import-sort', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
-    'import/default': 'off',
-    'import/export': 'off',
-    'import/first': 'error',
-    'import/namespace': 'off',
-    'import/newline-after-import': 'error',
-    'import/no-absolute-path': 'error',
-    'import/no-duplicates': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'import/no-unresolved': 'off',
-    'import/order': 'off',
-    'no-console': ['warn', { allow: ['info', 'warn', 'error'] }],
-    'no-else-return': 'warn',
-    'no-var': 'error',
-    'object-shorthand': 'warn',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error'
-  }
+  extends: ['@forgedev-br/eslint-config/vue']
 }
