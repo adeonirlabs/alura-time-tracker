5b06030ea2e302a32086a8cfab3b41591e10a078
Adeonir Kohl <adeonir@gmail.com>
2024-08-27 15:53:20 -0300

    Create task list component

---
+++ src/components/app/TaskList.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import TaskItem from '@/components/ui/TaskItem.vue'
+
+export default {
+  name: 'TaskList',
+  components: {
+    TaskItem,
+  },
+}
+</script>
+
+<template>
+  <article class="flex flex-col rounded-lg bg-white shadow">
+    <!-- <header class="rounded-t-lg bg-indigo-200 px-4 py-2 font-semibold text-indigo-900">Today</header> -->
+    <TaskItem />
+    <TaskItem />
+  </article>
+</template>
