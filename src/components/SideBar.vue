4c0fd7dcbeac2b49702c980f06c4d3cfc7832d36
Adeonir Kohl <adeonir@gmail.com>
2024-08-27 08:55:52 -0300

    Create sidebar component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import AppLogo from '@/components/AppLogo.vue'
+
+export default {
+  name: 'SideBar',
+  components: {
+    AppLogo,
+  },
+}
+</script>
+
+<template>
+  <header class="bg-indigo-900 h-auto md:h-screen w-full p-10 md:p-4">
+    <h1>
+      <AppLogo />
+    </h1>
+  </header>
+</template>
