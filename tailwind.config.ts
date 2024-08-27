ba012bfe8c2f58a37cf974f07b34b21358267062
Adeonir Kohl <adeonir@gmail.com>
2024-08-27 08:55:06 -0300

    Add new colors to tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,23 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mango: {
+          50: '#fff9eb',
+          100: '#feeec7',
+          200: '#fddb8a',
+          300: '#fcc34f',
+          400: '#fbab24',
+          500: '#f5880b',
+          600: '#d96306',
+          700: '#b44309',
+          800: '#92330e',
+          900: '#782b0f',
+          950: '#451403',
+        },
+      }
+    },
   },
   plugins: [],
 } satisfies Config
