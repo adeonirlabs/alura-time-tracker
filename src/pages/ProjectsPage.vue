d8015044739f7acc707b87dc93a7ec30bd572961
Adeonir Kohl <adeonir@gmail.com>
2024-08-28 18:18:03 -0300

    Create projects page

---
+++ src/pages/ProjectsPage.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+export default {
+  name: 'ProjectsPage',
+}
+</script>
+
+<template>
+  <section class="p-6">
+    <h1 class="text-foreground">Projects</h1>
+  </section>
+</template>
