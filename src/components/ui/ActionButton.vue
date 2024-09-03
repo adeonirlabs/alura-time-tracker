16df4a4d6f6624e9416cf8e10d80e9a371722dce
Adeonir Kohl <adeonir@gmail.com>
2024-09-03 15:14:19 -0300

    Update button variants

--- src/components/ui/ActionButton.vue
+++ src/components/ui/ActionButton.vue
@@ -1,22 +1,31 @@
 <script lang="ts">
+import type { ClassValue } from 'clsx'
 import type { PropType } from 'vue'
+
+import { cn } from '@/utils/classname'
 
 export default {
   name: 'ActionButton',
   props: {
     variant: {
-      type: String as PropType<'primary' | 'secondary'>,
+      type: String as PropType<'primary' | 'secondary' | 'destructive'>,
       required: true,
     },
   },
   computed: {
-    buttonClass() {
-      return {
-        'bg-primary text-primary-foreground hover:not:disabled:bg-primary/90 focus-visible:outline-primary':
-          this.variant === 'primary',
-        'bg-destructive text-destructive-foreground hover:not:disabled:bg-destructive/90 focus-visible:outline-destructive':
-          this.variant === 'secondary',
-      }
+    buttonVariants() {
+      return cn(
+        'flex items-center justify-center gap-2 text-nowrap rounded p-2 text-sm font-medium',
+        'shadow-sm transition-all focus-visible:outline focus-visible:outline-2',
+        'focus-visible:outline-offset-2 disabled:cursor-not-allowed disabled:opacity-70',
+        {
+          'bg-primary text-primary-foreground enabled:hover:bg-primary/90': this.variant === 'primary',
+          'bg-secondary text-secondary-foreground ring-1 ring-inset ring-secondary-foreground hover:bg-secondary/50':
+            this.variant === 'secondary',
+          'bg-destructive text-destructive-foreground enabled:hover:bg-destructive/90': this.variant === 'destructive',
+        },
+        this.$attrs.class as ClassValue,
+      )
     },
   },
 }
@@ -24,8 +33,7 @@ export default {
 
 <template>
   <button
-    :class="buttonClass"
-    class="flex w-24 items-center justify-center gap-2 rounded p-2 text-sm font-medium shadow-sm transition focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed disabled:opacity-70"
+    :class="buttonVariants"
     v-bind="$attrs"
   >
     <slot />
