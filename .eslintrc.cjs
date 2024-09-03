a8e556d9892d76c47994b06637fbcf3227250194
Adeonir Kohl <adeonir@gmail.com>
2024-09-03 14:47:43 -0300

    Update packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,4 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  root: true,
   extends: ['@forgedev-br/eslint-config/vue'],
-  rules: {
-    'vue/max-attributes-per-line': [
-      'warn',
-      {
-        singleline: {
-          max: 4,
-        },
-      },
-    ],
-    'vue/html-indent': [
-      'error',
-      2,
-      {
-        baseIndent: 1,
-      },
-    ],
-  },
 }
