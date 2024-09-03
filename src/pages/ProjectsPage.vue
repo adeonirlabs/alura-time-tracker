0adc7c75151f4fd4509a70d1cf13754b0989ef22
Adeonir Kohl <adeonir@gmail.com>
2024-09-03 15:15:34 -0300

    Add new project button on projects page

--- src/pages/ProjectsPage.vue
+++ src/pages/ProjectsPage.vue
@@ -1,11 +1,19 @@
 <script lang="ts">
+import ActionButton from '../components/ui/ActionButton.vue'
+
 export default {
   name: 'ProjectsPage',
+  components: {
+    ActionButton,
+  },
 }
 </script>
 
 <template>
   <section class="p-6">
-    <h1 class="text-foreground">Projects</h1>
+    <header class="flex items-center justify-between">
+      <h1 class="text-3xl text-muted-foreground">Projects</h1>
+      <ActionButton variant="secondary">Criar novo projeto</ActionButton>
+    </header>
   </section>
 </template>
